a78428c1459928b85a8dc4f42aa84cd22a5dc26c
Ahmed-Asker58 <130188023+Ahmed-Asker58@users.noreply.github.com>
2023-04-08 21:53:50 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,190 @@
+from flask import Flask, request, render_template, url_for, jsonify
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
+from skimage import feature
+from imutils import paths
+import numpy as np
+import cv2
+import os
+from PIL import Image
+
+app = Flask(__name__)
+
+#compute the histogram of every image to extract featues
+def quantify_image(image):
+	# compute the histogram of oriented gradients feature vector for
+	# the input image
+	features = feature.hog(image, orientations=9,
+		pixels_per_cell=(10, 10), cells_per_block=(2, 2),
+		transform_sqrt=True, block_norm="L1")
+	# return the feature vector
+	return features
+
+
+
+#train on the data set
+def Train(path):
+	
+	# grab the list of images in the input directory, then initialize
+	# the list of data (i.e., images) and class labels
+	imagePaths = list(paths.list_images(path))
+	data = []
+	labels = []
+	# loop over the image paths [healthy,parkinson]
+	for imagePath in imagePaths:
+		# extract the class label from the filename
+		label = imagePath.split(os.path.sep)[-2]
+		# load the input image, convert it to grayscale, and resize
+		# it to 200x200 pixels, ignoring aspect ratio
+		image = cv2.imread(imagePath)
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		image = cv2.resize(image, (200, 200))
+		# threshold the image such that the drawing appears as white
+		# on a black background
+		image = cv2.threshold(image, 0, 255,
+			cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+		# quantify the image
+		features = quantify_image(image)
+		# update the data and labels lists, respectively
+		data.append(features)
+		labels.append(label)
+	# return the data and labels
+	return (np.array(data), np.array(labels))
+
+
+def TrainTest(path):
+	# grab the list of images in the input directory, then initialize
+	# the list of data (i.e., images) and class labels
+	imagePaths = list(paths.list_images(path))
+	data = []
+	
+	# loop over the image paths [healthy,parkinson]
+	for imagePath in imagePaths:
+		# extract the class label from the filename
+		label = imagePath.split(os.path.sep)[-2]
+		# load the input image, convert it to grayscale, and resize
+		# it to 200x200 pixels, ignoring aspect ratio
+		image = cv2.imread(imagePath)
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		image = cv2.resize(image, (200, 200))
+		# threshold the image such that the drawing appears as white
+		# on a black background
+		image = cv2.threshold(image, 0, 255,
+			cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+		# quantify the image
+		features = quantify_image(image)
+		# update the data and labels lists, respectively
+		data.append(features)
+		
+	# return the data and labels
+	return (np.array(data))
+
+
+# define the path to the training and testing directories
+trainingPath = r"D:\dataset2\wave\training"
+testingPath = r"D:\dataset2\wave\testing"
+
+#training the data
+(trainX, trainY) = Train(trainingPath)
+(testX) = TrainTest(testingPath)
+# encode the labels as integers
+le = LabelEncoder()
+trainY = le.fit_transform(trainY)
+
+
+
+# loop over the number of trials to run
+for i in range(0, 5):
+	# train the model
+	
+	model = RandomForestClassifier(n_estimators=100)
+	model.fit(trainX, trainY)
+	# make predictions on the testing data and initialize a dictionary
+	# to store our computed metrics
+	predictions = model.predict(testX)
+	
+
+
+
+
+def Predict(image):
+    results = []
+	# load the testing image, clone it, and resize it
+	# pre-process the image in the same manner we did earlier
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.resize(image, (200, 200))
+    image = cv2.threshold(image, 0, 255,
+		cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+	
+    features = quantify_image(image)
+    preds = model.predict([features])
+    label = le.inverse_transform(preds)[0]
+    results.append(label)
+    if("parkinson" in results):
+     return "parkinson"
+    else:
+     return "healthy"
+    
+
+def preprossing(image):
+    image=Image.open(image)
+    image_arr = np.array(image.convert('RGB'))
+   
+    return image_arr
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', appName="Parkinson's Detection")
+
+
+
+@app.route('/predictApi', methods=["POST"])
+def api():
+    # Get the image from post request
+    try:
+        if 'fileup' not in request.files:
+            return "Please try again. The Image doesn't exist"
+        image = request.files.get('fileup')
+        image = preprossing(image)
+        print("Model predicting ...")
+        result = Predict(image)
+        print("Model predicted")
+        print(result)
+        return jsonify({'prediction': result})
+    except:
+        return jsonify({'Error': 'Error occur'})
+    
+
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    print("run code")
+    if request.method == 'POST':
+        # Get the image from post request
+        print("image loading....")
+        image = request.files['fileup']
+        print("image loaded....")
+        image = preprossing(image)
+        result = Predict(image)
+        print("predicted ...")
+        print(result)
+
+        return render_template('index.html', prediction=result, appName="Parkinson's Detection")
+    else:
+        return render_template('index.html',appName="Parkinson's Detection")
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
